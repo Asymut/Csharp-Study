dee2edc85423b27808c7d2112130965b5a885151
Asymut <115036683+Asymut@users.noreply.github.com>
2024-04-02 11:50:20 +0200

    WinForms1

---
+++ WinForms1/WinForms1/WinForms1/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinForms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text.ToString();    
+        }
+    }
+}
